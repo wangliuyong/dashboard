6a24176f193f614b46f4729a54bf0fc70eaf2d0d
“wly” <1355498705@qq.com>
2019-08-19 20:34:59 +0800

    配置固定行高

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ export class DashboardComponent implements OnInit {
   public options: DashboardConfig;
   public items: DashboardWidget[];
   resizeEvent: EventEmitter<any> = new EventEmitter<any>();
+  i = 0;
 
   protected subscription: Subscription;
 
@@ -18,11 +19,93 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.options = {
+      /* 所有配置
+      gridType?: gridTypes;
+      fixedColWidth?: number;
+      fixedRowHeight?: number;
+      keepFixedHeightInMobile?: boolean;
+      keepFixedWidthInMobile?: boolean;
+      setGridSize?: boolean;
+      compactType?: compactTypes;
+      mobileBreakpoint?: number;
+      minCols?: number;
+      maxCols?: number;
+      minRows?: number;
+      maxRows?: number;
+      defaultItemCols?: number;
+      defaultItemRows?: number;
+      maxItemCols?: number;
+      maxItemRows?: number;
+      minItemCols?: number;
+      minItemRows?: number;
+      minItemArea?: number;
+      maxItemArea?: number;
+      margin?: number;
+      outerMargin?: boolean;
+      outerMarginTop?: number | null;
+      outerMarginRight?: number | null;
+      outerMarginBottom?: number | null;
+      outerMarginLeft?: number | null;
+      useTransformPositioning?: boolean;
+      scrollSensitivity?: number | null;
+      scrollSpeed?: number;
+      initCallback?: (gridster: GridsterComponentInterface) => void;
+      destroyCallback?: (gridster: GridsterComponentInterface) => void;
+      gridSizeChangedCallback?: (gridster: GridsterComponentInterface) => void;
+      itemChangeCallback?: (item: GridsterItem, itemComponent: GridsterItemComponentInterface) => void;
+      itemResizeCallback?: (item: GridsterItem, itemComponent: GridsterItemComponentInterface) => void;
+      itemInitCallback?: (item: GridsterItem, itemComponent: GridsterItemComponentInterface) => void;
+      itemRemovedCallback?: (item: GridsterItem, itemComponent: GridsterItemComponentInterface) => void;
+      itemValidateCallback?: (item: GridsterItem) => boolean;
+      draggable?: Draggable;
+      resizable?: Resizable;
+      swap?: boolean;
+      pushItems?: boolean;
+      disablePushOnDrag?: boolean;
+      disablePushOnResize?: boolean;
+      disableAutoPositionOnConflict?: boolean;
+      pushDirections?: PushDirections;
+      pushResizeItems?: boolean;
+      displayGrid?: displayGrids;
+      disableWindowResize?: boolean;
+      disableWarnings?: boolean;
+      scrollToNewItems?: boolean;
+      disableScrollHorizontal?: boolean;
+      disableScrollVertical?: boolean;
+      enableEmptyCellClick?: boolean;
+      enableEmptyCellContextMenu?: boolean;
+      enableEmptyCellDrop?: boolean;
+      enableEmptyCellDrag?: boolean;
+      emptyCellClickCallback?: (event: MouseEvent, item: GridsterItem) => void;
+      emptyCellContextMenuCallback?: (event: MouseEvent, item: GridsterItem) => void;
+      emptyCellDropCallback?: (event: MouseEvent, item: GridsterItem) => void;
+      emptyCellDragCallback?: (event: MouseEvent, item: GridsterItem) => void;
+      emptyCellDragMaxCols?: number;
+      emptyCellDragMaxRows?: number;
+      ignoreMarginInRow?: boolean;
+      api?: {
+        resize?: () => void,
+        optionsChanged?: () => void,
+        getNextPossiblePosition?: (newItem: GridsterItem) => boolean,
+        getFirstPossiblePosition?: (item: GridsterItem) => GridsterItem,
+        getLastPossiblePosition?: (item: GridsterItem) => GridsterItem,
+      };
+      [propName: string]: any;
+      */
       disablePushOnDrag: true,
       draggable: { enabled: true },
-      gridType: GridType.Fit,
+      gridType: GridType.Fixed, // 可以固定定元素高度宽度，像素为单位
       resizable: { enabled: true },
-      displayGrid: DisplayGrid.None,
+      displayGrid: DisplayGrid.Always,
+      setGridSize: true,
+      keepFixedHeightInMobile: true,
+      // fixedColWidth: 250,
+      fixedRowHeight: 50,
+      margin: 24,
+      maxCols: 6,
+      minCols: 6,
+      maxRows: 100, // 行
+      minRows: 20, // 行
       itemResizeCallback: (item) => {
         // update DB with new size
         // send the update to widgets
@@ -32,6 +115,7 @@ export class DashboardComponent implements OnInit {
       itemChangeCallback: (item) => {
         // 更新widget, 重新赋值操作
         console.log(item);
+        console.log(this.i++);
       }
     };
     this.items = [
@@ -40,7 +124,7 @@ export class DashboardComponent implements OnInit {
         name: 'Timeline',
         component: 'timeline',
         cols: 2,
-        rows: 2,
+        rows: 3,
         y: 2,
         x: 2
       },
